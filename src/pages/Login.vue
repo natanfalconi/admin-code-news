9ec8ac9fcab97ea562d31f0b22b100805274c124
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-07-04 08:46:42 -0300

    ajuste de autenticacao

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import useAuthUser from 'src/composables/UseAuthUser'
 import { useRouter } from 'vue-router'
 import useNotify from 'src/composables/useNotify'
@@ -31,12 +31,18 @@ export default defineComponent({
   name: 'PageLogin',
   setup() {
     const router = useRouter()
-    const { login } = useAuthUser()
+    const { login, isLoggedIn } = useAuthUser()
     const { notifyError, notifySuccess } = useNotify()
 
     const form = ref({
       email: '',
       password: ''
+    })
+
+    onMounted(() => {
+      if (isLoggedIn) {
+        router.push({ name: 'me' })
+      }
     })
 
     const handleLogin = async () => {
