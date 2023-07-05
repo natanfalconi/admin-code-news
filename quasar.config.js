89cceda48f27a8a284dc618463fb895a628872d9
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-07-05 09:06:57 -0300

    test config quasar

--- quasar.config.js
+++ quasar.config.js
@@ -52,6 +52,14 @@ module.exports = configure(function (/* ctx */) {
         browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node16",
       },
+      extendWebpack(cfg) {
+        cfg.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'unlazy-loader',
+          exclude: /node_modules/
+        });
+      },
       env: {
         SUPABASE_URL: 'https://hnagncamzhjfqebflwfn.supabase.co',
         SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuYWduY2FtemhqZnFlYmZsd2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgzODQ2NzcsImV4cCI6MjAwMzk2MDY3N30.CXMbfDDk20_YSTeOITeNnrIT7w6U8gdxm314megKww8'
